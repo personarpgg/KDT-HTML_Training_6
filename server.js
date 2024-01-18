fdf1ce15695b8d77084055a04b9c1cb4b66ea329
Garry <personarpg7501@gmail.com>
2024-01-18 23:37:20 +0900

    check : server.js완료

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const expres = require('express')//express 형태
-const fs = require('fs');
+const expres = require("express"); //express 형태
+const fs = require("fs");
 
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-
-
+app.listen(port, () => {
+  console.log(`시작 ${port}`);
 });
