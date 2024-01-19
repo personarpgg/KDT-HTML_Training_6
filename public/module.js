f59cf9fd7dcba26137dfb787b61f253398cd4dc2
Garry <personarpg7501@gmail.com>
2024-01-19 23:54:29 +0900

    check : server.js에서 모듀 불러오기 실패 module.js로 변경하여 테스트 시작

---
+++ public/module.js
@@ -0,0 +1,13 @@
+const root = document.getElementById("root");
+
+//콘솔에도 안뜸
+fetch("/public/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const props = data.paragraphs;
+    console.log(props);
+
+    // 여기에 데이터를 처리하거나 화면에 표시하는 로직을 추가하면 됩니다.
+  })
+
+  .catch((error) => console.error("데이터 못 불러옴", error));
--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const root = document.getElementById("root");
-
-//이거 쓰려면 정책 문제로 8080 써야함
-fetch('/public/data.json')
-.then(response => response.json())
-.then(data => {
-  const props = data.paragraphs;
-  console.log(props); // 올바른 콘솔 로그 문법을 사용
-  
-  // 여기에 데이터를 처리하거나 화면에 표시하는 로직을 추가하면 됩니다.
-})
-
-    .catch(error => console.error('데이터 못 불러옴', error));
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:3000/`);
 });
 
 // 모듈로 내보내기
