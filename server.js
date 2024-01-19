cd6d95b7c305bca04e83cafda855e00218d76be8
Garry <personarpg7501@gmail.com>
2024-01-19 23:47:52 +0900

    check : server.js로 script 코드 이동

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+const root = document.getElementById("root");
+
+//이거 쓰려면 정책 문제로 8080 써야함
+fetch('/public/data.json')
+.then(response => response.json())
+.then(data => {
+  const props = data.paragraphs;
+  console.log(props); // 올바른 콘솔 로그 문법을 사용
+  
+  // 여기에 데이터를 처리하거나 화면에 표시하는 로직을 추가하면 됩니다.
+})
+
+    .catch(error => console.error('데이터 못 불러옴', error));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
